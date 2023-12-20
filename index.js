4e1f4ac41751e0af325b2e88f5661185c62f16d7
CarolineBellaLane <carolinelane@live.co.uk>
2023-12-20 19:45:07 +0000

    adds jest and makes a succesful test request

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Manager = require('./lib/Manager.js');
+const Engineer = require('./lib/Engineer.js');
+const Employee = require('./lib/Employee.js');
+const Intern = require("./lib/Intern.js");
 const inquirer = require("inquirer");
-const path = require("path");
 const fs = require("fs");
+const path = require("path");
+const render = require("./src/page-template.js");
+
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
@@ -79,7 +80,7 @@ const promptManager = () => {
     team.push(manager);
 
     // Call the function to get additional team members????
-    // promptTeamMembers();
+    promptTeamMembers();
   });
 };
 
